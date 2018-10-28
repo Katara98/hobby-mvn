159cf48db0d1d9f9f069408663ed4d2649f0ccb5
Katara98 <33484724+Katara98@users.noreply.github.com>
2018-10-28 19:30:36 +0200

    Initial commit

---
+++ src/main/java/com/nadya/Hobby.java
@@ -0,0 +1,111 @@
+package com.nadya;
+
+public class Hobby {
+    private String name;
+    private char priority;
+    private byte currentWorks;
+    private short plannedWorks;
+    private int hours;
+    private long finishedWorks;
+    private float earnedMoney;
+    private double spentMoney;
+    private boolean isActive;
+
+    Hobby() {
+    }
+
+    Hobby(String name) {
+        this.name=name;
+    }
+
+    Hobby(String name, int hours) {
+        this.name = name;
+        this.hours = hours;
+    }
+
+    public char getPriority() {
+        return priority;
+    }
+
+    public void setPriority(char priority) {
+        this.priority = priority;
+    }
+
+    public byte getCurrentWorks() {
+        return currentWorks;
+    }
+
+    public void setCurrentWorks(byte currentWorks) {
+        this.currentWorks = currentWorks;
+    }
+
+    public short getPlannedWorks() {
+        return plannedWorks;
+    }
+
+    public void setPlannedWorks(short plannedWorks) {
+        this.plannedWorks = plannedWorks;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public long getFinishedWorks() {
+        return finishedWorks;
+    }
+
+    public void setFinishedWorks(long finishedWorks) {
+        this.finishedWorks = finishedWorks;
+    }
+
+    public float getEarnedMoney() {
+        return earnedMoney;
+    }
+
+    public void setEarnedMoney(float earnedMoney) {
+        this.earnedMoney = earnedMoney;
+    }
+
+    public double getSpentMoney() {
+        return spentMoney;
+    }
+
+    public void setSpentMoney(double spentMoney) {
+        this.spentMoney = spentMoney;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String tellAboutHobby() {
+        return "main.Hobby{" +
+                "name='" + name + '\'' +
+                ", hours=" + hours +
+                ", priority=" + priority +
+                ", finishedWorks=" + finishedWorks +
+                ", currentWorks=" + currentWorks +
+                ", plannedWorks=" + plannedWorks +
+                ", earnedMoney=" + earnedMoney +
+                ", spentMoney=" + spentMoney +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
